cbf87baa306cb571d6671f08a693b6b7683ce7f3
Roald Ignacio <me@mac.com>
2019-12-04 22:45:01 +1300

    MongoDB Connected

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,12 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Jenssegers\Mongodb\MongodbServiceProvider::class);
+
+// $app->withFacades();
+
+// $app->withEloquent();
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
