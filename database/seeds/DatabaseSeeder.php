e8e2dae82d96ed7ee8cfaf3ee594b0e6f3667854
Roald Ignacio <me@mac.com>
2019-12-07 07:39:22 +1300

    TODO: Add Database Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // Delete users first
+        // DB::collection('users')->delete();
+
+        // Insert to database
+        $user = DB::collection('users')->insert([
+            'name' => 'Jap Ignacio',
+            'email' => 'roaldjap@gmail.com',
+            'password' => 'testtest'
+        ]);
+
+        $post = DB::collection('post')->insert([
+            'title' => "Awesome Article", 
+            'body' => "Here is some Sample content please see.", 
+            'user_id' => $user->id
+        ]);
+
+        $post_image = DB::collection('post')->insert([
+            'title' => "Awesome Article", 
+            'body' => "Here is some Sample content please see.", 
+            'user_id' => $user->id
+        ]);
     }
 }
