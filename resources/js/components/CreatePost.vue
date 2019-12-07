a346af92a4f132b45531ee4f571b614bd2c16a42
Roald Ignacio <me@mac.com>
2019-12-07 14:18:35 +1300

    Added auto refresh component after creation of Post

--- resources/js/components/CreatePost.vue
+++ resources/js/components/CreatePost.vue
@@ -118,7 +118,7 @@ export default {
             this.status = true;
             this.showNotification('Post Successfully created');
             this.isCreatingPost = false;
-            this.getAllPosts();
+            this.$store.dispatch('getAllPosts');
         });
 
       }
@@ -132,30 +132,3 @@ export default {
 
 
 </script>
-
-<style>
-  .avatar-uploader .el-upload{
-    border: 1px solid dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover{
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon{
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar{
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
-
-</style>
